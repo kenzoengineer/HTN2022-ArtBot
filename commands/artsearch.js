bf327cffcbd4aee27aa57303ad4a531b83c48317
Ken Jiang <kenzoengineer@gmail.com>
2022-09-14 17:27:48 -0400

    test commit cuz it was broken before

--- commands/artsearch.js
+++ commands/artsearch.js
@@ -5,15 +5,25 @@ const { getArtById } = require("../utils/artEmbed.js");
 
 const TRUNCATION = 30;
 
+/**
+ * /artsearch <query>
+ * 
+ * Given a query, we use the api to search their artworks. We then return max 10 in an
+ * embed and let the user select which artwork they want. When the user responds, we output
+ * the corresponding artwork.
+ */
+
 module.exports = {
+	// create a new slash command with a string option for the search query
 	data: new SlashCommandBuilder()
 		.setName("artsearch")
 		.setDescription("Search for an art piece")
 		.addStringOption(option => option.setName("search").setDescription("Search query").setRequired(true)),
 	async execute(interaction) {
+		// get the query
 		const q = interaction.options.getString("search");
+		// search using the API, returning a maximum of 10
 		fetch(`https://api.artic.edu/api/v1/artworks/search?fields=id,title,artist_title&limit=10&q=${q}`, {
-				method: "POST",
 				headers: { "Content-Type": "application/json" },
 			}
 		)
